7af4c0a5c08d2caa53ed8715c257396342fd1063
subhashiscse <smollickcseiu@gmail.com>
2024-05-30 23:01:43 +0600

    Image Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import { SlideInterface } from './image-slider/types/slider.interface';
 })
 export class AppComponent {
   slides: SlideInterface[] = [
-    { url: '/assets/images/slider-1.jpg', title: 'beach' },
-    { url: '/assets/images/slider-2.jpg', title: 'boat' },
-    { url: '/assets/images/slider-3.jpg', title: 'forest' },
-    { url: '/assets/images/slider-4.jpg', title: 'city' },
-    { url: '/assets/images/slider-5.jpg', title: 'city' },
-    { url: '/assets/images/slider-6.jpg', title: 'city' }
+    { url: './assets/images/slider-1.jpg', title: 'beach' },
+    { url: './assets/images/slider-2.jpg', title: 'boat' },
+    { url: './assets/images/slider-3.jpg', title: 'forest' },
+    { url: './assets/images/slider-4.jpg', title: 'city' },
+    { url: './assets/images/slider-5.jpg', title: 'city' },
+    { url: './assets/images/slider-6.jpg', title: 'city' }
   ];
   navigationList :any = NavigationMenuList;
 }
