1e900099afd3bbb68bae628f638e115e3082e7fa
subhashiscse <smollickcseiu@gmail.com>
2024-05-30 22:31:44 +0600

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cpu-idpc'),
+      dir: require('path').join(__dirname, './coverage/cpuidpc'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cpu-idpc'`, () => {
+  it(`should have as title 'cpuidpc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cpu-idpc');
+    expect(app.title).toEqual('cpuidpc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cpu-idpc app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cpuidpc app is running!');
   });
 });
