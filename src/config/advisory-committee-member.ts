b2f89a0f4dc032151576ffd0014027782b6ae52a
subhashismollick <smollickcseiu@gmail.com>
2024-06-05 00:48:27 +0600

    Update Images

--- src/config/advisory-committee-member.ts
+++ src/config/advisory-committee-member.ts
@@ -1,17 +1,26 @@
-export const AdvisoryCommitteeMemberList = [
-    {
-        Name:'Dr. Robiul Hoque',
-        Designation: 'Professor',
-        ImageUrl: './assets/images/person.jpg'
-    },
+export interface AdvisoryCommittee{
+    Name: string;
+    Email: string;
+    Designation: string;
+    ImageUrl: string;
+}
+export const AdvisoryCommitteeMemberList: AdvisoryCommittee[] = [
     {
         Name:'Ibrahim Abdullah',
+        Email:'',
         Designation: 'Professor',
-        ImageUrl: './assets/images/person.jpg'
+        ImageUrl: './assets/images/ibrahim_sir.jpg'
+    },
+    {
+        Name:'Dr. Robiul Hoque',
+        Email:'',
+        Designation: 'Professor',
+        ImageUrl: './assets/images/robiul_sir.jpg'
     },
     {
         Name:'Dr. Mojahidul Islam',
+        Email:'',
         Designation: 'Assistant Professor',
-        ImageUrl: './assets/images/person.jpg'
+        ImageUrl: './assets/images/mojahid_sir.jpg'
     }
 ]
